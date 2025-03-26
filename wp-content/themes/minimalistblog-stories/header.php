caadce2de7a16769e3c80798595ea770850b6a1b
Aljohn Dumlao <aljohndumlaobasc@gmail.com>
2025-03-26 12:52:50 +0800

    Task 2

--- wp-content/themes/minimalistblog-stories/header.php
+++ wp-content/themes/minimalistblog-stories/header.php
@@ -7,6 +7,8 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
